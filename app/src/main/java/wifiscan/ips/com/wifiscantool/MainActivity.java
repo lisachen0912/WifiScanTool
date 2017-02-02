30af60f3d55946a931f8802832e5009324f638d6
lisachen <lisa12734@gmail.com>
2017-02-02 10:16:30 +0900

    Upload first version
    Scan surrounding wifi BSSID and signal strength.

---
+++ app/src/main/java/wifiscan/ips/com/wifiscantool/MainActivity.java
@@ -0,0 +1,121 @@
+package wifiscan.ips.com.wifiscantool;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    WifiManager wifi;
+    ListView lv;
+    TextView textStatus;
+    Button buttonScan;
+    int size = 0;
+    List<ScanResult> results;
+
+    String ITEM_KEY = "key";
+    ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
+    SimpleAdapter adapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        buttonScan.setOnClickListener(this);
+        lv = (ListView)findViewById(R.id.list);
+
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wifi.isWifiEnabled() == false)
+        {
+            Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
+            wifi.setWifiEnabled(true);
+        }
+        this.adapter = new SimpleAdapter(this, arraylist, R.layout.row, new String[] { ITEM_KEY }, new int[] { R.id.list_value });
+        lv.setAdapter(this.adapter);
+
+        registerReceiver(mWifiScanReceiver,
+                new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    0x12345);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+            Log.d("Lisa", "request permission");
+        } else {
+            Log.d("Lisa", "permission already granted");
+        }
+    }
+
+
+    private final BroadcastReceiver mWifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                //List<ScanResult> mScanResults = wifi.getScanResults();
+                // clean list
+                Log.d("Lisa", "== clear list ==");
+                arraylist.clear();
+                size = 0;
+
+                results = wifi.getScanResults();
+                size = results.size();
+                Log.d("Lisa", "receive intent!!" + intent.toString());
+                try
+                {
+
+                    Log.d("Lisa", "-- List -- " + size);
+                    size = size - 1;
+                    while (size >= 0)
+                    {
+                        HashMap<String, String> item = new HashMap<String, String>();
+                        ScanResult scanItem = results.get(size);
+                        item.put(ITEM_KEY, scanItem.SSID + " /// " + scanItem.BSSID + " /// " + scanItem.centerFreq0 +
+                                " /// " + scanItem.frequency + " /// " + scanItem.level);
+                        Log.d("Lisa", scanItem.SSID + " " + scanItem.level);
+
+                        arraylist.add(item);
+                        size--;
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.d("Lisa", "error!! " + e.toString());
+                }
+            }
+        }
+    };
+
+    public void onClick(View view)
+    {
+        wifi.startScan();
+        Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
+    }
+}
